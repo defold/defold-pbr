130fdce1948b6985f02e3ce8ce37c9d165d9c42a
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-04-29 12:50:12 +0200

    Pipeline fixes + bugfix

--- defold-pbr/core.lua
+++ defold-pbr/core.lua
@@ -1,5 +1,22 @@
 local helpers = require 'defold-pbr/scripts/helpers'
 
+local function light_template(light_type)
+	return {
+		position  = vmath.vector3(),
+		direction = vmath.vector3(),
+		color     = vmath.vector3(1, 1, 1),
+		intensity = 1,
+		type      = light_type,
+	}
+end
+
+local function extend(a, b)
+	for k, v in pairs(b) do
+		a[k] = b[k]
+	end
+	return a
+end
+
 local M = {}
 
 M.initialize = function(params)
@@ -8,17 +25,14 @@ M.initialize = function(params)
 	ctx.texture_irradiance = helpers.make_irradiance_texture(ctx.params.irradiance.width, ctx.params.irradiance.height)
 	ctx.texture_prefilter  = helpers.make_prefilter_texture(ctx.params.prefilter.width, ctx.params.prefilter.height, ctx.params.prefilter.mipmaps) 
 	ctx.texture_brdf_lut   = helpers.make_brdf_lut("/defold-pbr/assets/brdf_lut.bin", 512, 512)
-
-	print(ctx.texture_irradiance)
-	print(ctx.texture_prefilter)
-	print(ctx.texture_brdf_lut)
 	
-	ctx.handle_irradiance  = resource.get_texture(ctx.texture_irradiance)
-	ctx.handle_prefilter   = resource.get_texture(ctx.texture_prefilter)
-	ctx.handle_brdf_lut    = resource.get_texture(ctx.texture_brdf_lut)
+	ctx.handle_irradiance  = resource.get_texture_info(ctx.texture_irradiance).handle
+	ctx.handle_prefilter   = resource.get_texture_info(ctx.texture_prefilter).handle
+	ctx.handle_brdf_lut    = resource.get_texture_info(ctx.texture_brdf_lut).handle
 	ctx.render_args        = {
 		lights = {},
 	}
+	
 	return ctx
 end
 
@@ -27,8 +41,15 @@ M.set_environment = function(ctx, env_params)
 end
 
 M.add_light_directional = function(ctx, l_params)
-	l_params.type = helpers.LIGHT_TYPE.DIRECTIONAL
-	table.insert(ctx.render_args.lights, l_params)
+	local l = extend(light_template(helpers.LIGHT_TYPE.DIRECTIONAL), l_params)
+	table.insert(ctx.render_args.lights, l)
+	msg.post("@render:", "add_light", l)
+end
+
+M.add_light_point = function(ctx, l_params)
+	local l = extend(light_template(helpers.LIGHT_TYPE.POINT), l_params)
+	table.insert(ctx.render_args.lights, l)
+	msg.post("@render:", "add_light", l)
 end
 
 M.update = function(ctx, update_params)
