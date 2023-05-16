0b6f4a4de789a75988f411851eeda5a73df35b12
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-05-16 12:56:29 +0200

    Add damaged helmet GLTF scene

---
+++ assets/scenes/DamagedHelmet_Build/gameobjects/node_damagedHelmet_-6514.go
@@ -0,0 +1,15 @@
+components {
+  id: "node_damagedHelmet_-6514"
+  component: "/assets/scenes/DamagedHelmet_Build/models/node_damagedHelmet_-6514.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
