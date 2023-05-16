0ab4f599a379a944da484f9cd73aa7aeae574cfd
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-05-16 18:05:01 +0200

    Remove environment-maps from gitignore

---
+++ assets/environment-maps/blue_photo_studio_2k/meta.lua
@@ -0,0 +1,8 @@
+return
+{
+    name            = "blue_photo_studio_2k.hdr",
+    path            = "/assets/environment-maps/blue_photo_studio_2k",
+    irradiance_size = 64,
+    prefilter_size  = 256,
+    brdf_lut_size   = 512,
+}
