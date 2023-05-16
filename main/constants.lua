0fcbb5da7e20e799c8f10c1febed4183f84b70b5
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-05-16 16:02:08 +0200

    Win32 updates

--- main/constants.lua
+++ main/constants.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.ENVIRONMENTS = {
-	require "assets/environment-maps/modern_buildings_2_2k/meta",
-	require "assets/environment-maps/solitude_interior_2k/meta",
+	-- Add 
+	require "assets/environment-maps/blue_photo_studio_2k/meta"
 }
 
 return M
