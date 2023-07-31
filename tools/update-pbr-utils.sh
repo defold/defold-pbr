8bd084d3f9e928790816ae0cb60a8d83abe5da69
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-07-31 12:43:12 +0200

    Add osx-python wrapper to plugin zip

--- tools/update-pbr-utils.sh
+++ tools/update-pbr-utils.sh
@@ -17,9 +17,10 @@ chmod +x ${OUTPUT_PATH}/pbr-utils-linux
 
 curl ${PBR_TOOLS_BASE_PATH}/pbr-utils-windows.exe --output-dir ${OUTPUT_PATH} -o pbr-utils-windows.exe -L
 
-zip -j ${PLUGINS_PATH}/x86_64-macos.zip ${OUTPUT_PATH}/pbr-utils-macos
-zip -j ${PLUGINS_PATH}/x86_64-win32.zip ${OUTPUT_PATH}/pbr-utils-windows.exe
-zip -j ${PLUGINS_PATH}/x86_64-linux.zip ${OUTPUT_PATH}/pbr-utils-linux
+# File Sync (FS) + put in root (j)
+zip -FSj ${PLUGINS_PATH}/x86_64-macos.zip ${OUTPUT_PATH}/pbr-utils-macos ${OUTPUT_PATH}/osx-python.sh
+zip -FSj ${PLUGINS_PATH}/x86_64-win32.zip ${OUTPUT_PATH}/pbr-utils-windows.exe
+zip -FSj ${PLUGINS_PATH}/x86_64-linux.zip ${OUTPUT_PATH}/pbr-utils-linux
 
 rm ${OUTPUT_PATH}/pbr-utils-macos
 rm ${OUTPUT_PATH}/pbr-utils-windows.exe
