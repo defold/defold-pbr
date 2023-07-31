eaec9a1d6ded5020200fe016c880a7d00a6e77a3
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-07-31 12:39:05 +0200

    Move tools to plugin zip folder

--- tools/update-pbr-utils.sh
+++ tools/update-pbr-utils.sh
@@ -6,6 +6,7 @@ export PBR_TOOLS_BASE_PATH=https://github.com/Jhonnyg/defold-pbr-utils/releases/
 export BLENDER_TOOLS_BASE_PATH=https://github.com/defold/blender-content-pipeline/archive/refs/heads/master.zip
 
 export TOOLS_PATH=defold-pbr/tools
+export PLUGINS_PATH=defold-pbr/plugins
 export OUTPUT_PATH=tools
 
 curl ${PBR_TOOLS_BASE_PATH}/pbr-utils-macos --output-dir ${OUTPUT_PATH} -o pbr-utils-macos -L
@@ -16,12 +17,16 @@ chmod +x ${OUTPUT_PATH}/pbr-utils-linux
 
 curl ${PBR_TOOLS_BASE_PATH}/pbr-utils-windows.exe --output-dir ${OUTPUT_PATH} -o pbr-utils-windows.exe -L
 
+zip -j ${PLUGINS_PATH}/x86_64-macos.zip ${OUTPUT_PATH}/pbr-utils-macos
+zip -j ${PLUGINS_PATH}/x86_64-win32.zip ${OUTPUT_PATH}/pbr-utils-windows.exe
+zip -j ${PLUGINS_PATH}/x86_64-linux.zip ${OUTPUT_PATH}/pbr-utils-linux
+
+rm ${OUTPUT_PATH}/pbr-utils-macos
+rm ${OUTPUT_PATH}/pbr-utils-windows.exe
+rm ${OUTPUT_PATH}/pbr-utils-linux
+
 curl ${BLENDER_TOOLS_BASE_PATH} --output-dir ${OUTPUT_PATH} -o blender-content-pipeline.zip -LJO
 unzip ${OUTPUT_PATH}/blender-content-pipeline.zip
 rm ${OUTPUT_PATH}/blender-content-pipeline.zip
 rm -rf ${TOOLS_PATH}/blender-content-pipeline
-
-mv ${OUTPUT_PATH}/pbr-utils-macos ${TOOLS_PATH}
-mv ${OUTPUT_PATH}/pbr-utils-windows.exe ${TOOLS_PATH}
-mv ${OUTPUT_PATH}/pbr-utils-linux ${TOOLS_PATH}
 mv blender-content-pipeline-master ${TOOLS_PATH}/blender-content-pipeline
